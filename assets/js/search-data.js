13641dbb467851797e805adb2d33ba65ebac67d3
shameekvats <63192223+shameekvats@users.noreply.github.com>
2025-02-19 08:56:04 +0000

    Deploying to gh-pages from @ shameekvats/shameekvats.github.io@1cee1c741ca1339ffdac334eb70671660c16bb4c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "cv",
+        },{id: "nav-short-cv",
+          title: "Short CV",
           description: "",
           section: "Navigation",
           handler: () => {
