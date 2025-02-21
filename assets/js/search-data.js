ee271fe1bdae7d5d3d1d8ac518f34debbdf548a5
shameekvats <63192223+shameekvats@users.noreply.github.com>
2025-02-21 09:21:23 +0000

    Deploying to gh-pages from @ shameekvats/shameekvats.github.io@ca6e7d15cb6b88d52aa5f0e7279b284073e03ed8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -41,7 +41,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-website-design",
+          section: "News",},{id: "projects-project-management",
+          title: 'project management',
+          description: "event organization &amp; management",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/project-mangement/";
+            },},{id: "projects-website-design",
           title: 'website design',
           description: "with dynamic layouts",
           section: "Projects",handler: () => {
